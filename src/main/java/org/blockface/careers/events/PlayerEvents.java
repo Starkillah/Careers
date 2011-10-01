7ff3c5f0a1e07b89e89a3baa17d7434274ee1d6f
SwearWord <theswearword@gmail.com>
2011-09-30 21:19:28 -0400

    Disable distance checking on multiworld teleport.

--- src/main/java/org/blockface/careers/events/PlayerEvents.java
+++ src/main/java/org/blockface/careers/events/PlayerEvents.java
@@ -48,7 +48,7 @@ public class PlayerEvents extends PlayerListener {
 
     @Override
     public void onPlayerTeleport(PlayerTeleportEvent event) {
-        if(event.getFrom().distance(event.getTo()) < 5) return;
+        if(event.getFrom().getWorld().equals(event.getTo().getWorld()) && event.getFrom().distance(event.getTo()) < 5) return;
         if(CrimeManager.isWanted(event.getPlayer().getName()) || PoisonManager.isPoisoned(event.getPlayer())) {
             Language.TELEPORT.bad(event.getPlayer());
             return;}
